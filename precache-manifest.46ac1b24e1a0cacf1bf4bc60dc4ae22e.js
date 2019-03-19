3ae397757986c8946fab7693c5d14a08a269e39d
Sydney Dunlap <sydney.dunlap2011@gmail.com>
2019-03-19 19:09:43 -0400

    Updates

---
+++ precache-manifest.46ac1b24e1a0cacf1bf4bc60dc4ae22e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "01f4c286af1d0fbe3d27",
+    "url": "/clicky-game/static/js/main.4c65870b.chunk.js"
+  },
+  {
+    "revision": "a145b351ca2c6996d84d",
+    "url": "/clicky-game/static/js/2.b739a309.chunk.js"
+  },
+  {
+    "revision": "01f4c286af1d0fbe3d27",
+    "url": "/clicky-game/static/css/main.32c6d5df.chunk.css"
+  },
+  {
+    "revision": "d3c855ae62c7c9649382ea2ea5f279d8",
+    "url": "/clicky-game/index.html"
+  }
+];
